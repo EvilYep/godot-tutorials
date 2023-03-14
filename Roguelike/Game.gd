1632ea73604885d284f1ee655c4298eb5589696e
EvilYep <guillaume.moneret@gmail.com>
2023-03-14 05:47:18 +0100

    centering game window :anchor:

---
+++ Roguelike/Game.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+#### BUILT-IN ####
+
+func _init() -> void:
+	var screen_size := OS.get_screen_size()
+	var window_size := OS.get_window_size()
+	
+	OS.set_window_position(screen_size * 0.5 - window_size * 0.5)
