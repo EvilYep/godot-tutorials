faab386f0ad0aeb1e4be2d853dca15e515c9c7a1
EvilYep <guillaume.moneret@gmail.com>
2023-04-04 01:40:16 +0200

    pixelate (with slider) :ice_cube:

---
+++ Shaders/Scenes/ShaderController.gd
@@ -0,0 +1,7 @@
+extends Camera2D
+
+#### SIGNAL RESPONSES ####
+
+func _on_v_slider_value_changed(value: float) -> void:
+	$CanvasLayer/ColorRect.get_material().set_shader_parameter("size_x", value)
+	$CanvasLayer/ColorRect.get_material().set_shader_parameter("size_y", value)
