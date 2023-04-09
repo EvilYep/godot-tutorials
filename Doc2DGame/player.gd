317c0542dd27f2dcf17de2c983bb8f3d783de59f
EvilYep <guillaume.moneret@gmail.com>
2023-04-09 20:53:27 +0200

    player :suspect:

---
+++ Doc2DGame/player.gd
@@ -0,0 +1,51 @@
+extends Area2D
+
+signal hit
+
+@export var speed: int = 400
+var screen_size: Vector2
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	hide()
+
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		animated_sprite.play()
+	else:
+		animated_sprite.stop()
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		animated_sprite.animation = "walk"
+		animated_sprite.flip_v = false
+		animated_sprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		animated_sprite.animation = "up"
+		animated_sprite.flip_v = velocity.y > 0
+
+func start(pos: Vector2) -> void:
+	position = pos
+	show()
+	collision_shape.disabled = false
+
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	collision_shape.set_deferred("disabled", true)
