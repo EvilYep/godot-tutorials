43a0a65ac95af644de1de1c4067c02ecc14c8b4b
EvilYep <guillaume.moneret@gmail.com>
2023-04-10 05:32:21 +0200

    mob scene :octocat:

---
+++ Doc3DGame/Mob.gd
@@ -0,0 +1,21 @@
+extends CharacterBody3D
+
+@export var min_speed: int = 10 # m per s
+@export var max_speed: int = 18
+
+func _physics_process(_delta: float) -> void:
+	move_and_slide()
+
+func initialize(start_position: Vector3, player_position: Vector3) -> void:
+	# position the mob at start_position then make it look at player
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	rotate_y(randf_range(-PI / 4, PI / 4))
+	
+	var random_speed = randi_range(min_speed, max_speed)
+	# speed
+	velocity = Vector3.FORWARD * random_speed
+	# direction
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+func _on_visibility_notifier_screen_exited() -> void:
+	queue_free()
