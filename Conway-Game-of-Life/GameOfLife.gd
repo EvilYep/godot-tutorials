c3e72df61ab9b82b63eb77bb46408eafcebbf4f4
EvilYep <guillaume.moneret@gmail.com>
2023-04-17 22:51:41 +0200

    setting up grid :white_check_mark:

--- Conway-Game-of-Life/GameOfLife.gd
+++ Conway-Game-of-Life/GameOfLife.gd
@@ -13,7 +13,6 @@ const TILE_SIZE: int = 128
 @onready var camera: Camera2D = $Camera2D
 
 var playing := false
-var temp_field := []
 
 func _ready() -> void:
 	var width_px = width * TILE_SIZE
@@ -23,14 +22,25 @@ func _ready() -> void:
 	camera.zoom = Vector2(1920, 1080) / Vector2(width_px, height_px)
 	
 	for x in range(width):
-		var temp = []
-		
 		for y in range(height):
-			_set_cell_state(CellState.DEAD, x, y)
-			temp.append(CellState.DEAD)
+			_set_cell_state(CellState.DEAD, Vector2(x, y))
 
-func _set_cell_state(alive: bool, x: int, y: int) -> void:
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_play"):
+		playing = not playing
+	
+	if event.is_action_pressed("click"):
+		var pos = local_to_map(get_local_mouse_position())
+		_toggle_cell_state(pos)
+
+func _set_cell_state(alive: bool, pos: Vector2) -> void:
 	if alive:
-		set_cell(0, Vector2i(x, y), 0, Vector2i(0, 0))
+		set_cell(0, pos, 0, Vector2i(0, 0))
 	else:
-		set_cell(0, Vector2i(x, y), 0, Vector2i(0, 0), 1)
+		set_cell(0, pos, 0, Vector2i(0, 0), 1)
+
+func _get_cell_state(pos: Vector2i) -> CellState:
+	return get_cell_tile_data(0, pos).get_custom_data("alive")
+
+func _toggle_cell_state(pos: Vector2) -> void:
+	_set_cell_state(1 - _get_cell_state(pos), pos)
